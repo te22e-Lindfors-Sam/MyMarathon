25ec3db3f62e86ddea24fce48f91f0e763195d7d
Sam Lindfors <sam.lindfors@elev.ga.ntig.se>
2024-02-15 17:20:10 +0100

    Added element to the front and made the users name show up instead of joe doe

--- public/navbar.js
+++ public/navbar.js
@@ -1,15 +1,19 @@
-var navbar;
-let navTop;
 document.addEventListener("DOMContentLoaded", event =>{
-    console.log("active");
+    // const app = firebase.app(); //only checks if the server is live
     window.addEventListener('scroll', stickynavbar);
     navbar = document.getElementById('navbar'); 
-    console.log(navbar);
     navTop = navbar.offsetTop;
+
+    //this part will throw an error if no user exists and with that it wont be able to add a new user
+    const storedValue = localStorage.getItem('user');
+    const storedObject = JSON.parse(storedValue);
+    var name = document.getElementById("userName");
+    name.textContent = storedObject.displayName; 
 });
 
 
-
+var navbar;
+let navTop;
 
 
 
